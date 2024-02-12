ef23ae90a2b0b0d7e0431b70b4768c2243aac8ba
Um6253 <um6253@srmist.edu.in>
2024-02-13 00:00:46 +0530

    [Mani]Adding:Creating a sample code of Student grade system

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NUnitTesting.Program;
+
+namespace NUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get percentage
+            var gradeCalculator = new GradeCalculator();
+
+            Console.Write("Enter the percentage : ");
+            var percentage = Convert.ToInt32(Console.ReadLine());
+           
+
+             var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+             Console.WriteLine($"Student Grade : {grade}");
+
+        }
+        public class GradeCalculator
+        {
+
+
+            public string GetGradeByPercentage(int percentage)
+            {
+
+                if (percentage >= 90 & percentage <= 100)
+                {
+                       return "A";
+                }
+               else if (percentage >= 80 && percentage <= 90)
+               {
+
+                    return "B";
+               }
+               else if (percentage >= 70 && percentage <= 80)
+               {
+
+                    return "C";
+               }
+                else if (percentage >= 60 && percentage <= 70)
+                { 
+                    return "D";
+                }
+
+                
+                else if (percentage >= 0 && percentage <= 60)
+                { 
+                    return "F";
+                }
+
+                else
+
+                { return "Invalid"; }
+            }
+        }
+    }
+}
+
