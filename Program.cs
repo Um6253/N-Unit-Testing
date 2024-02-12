a527bfb576caf667bd6f238a64f2efbcc06fb31d
Um6253 <um6253@srmist.edu.in>
2024-02-13 00:32:40 +0530

    [Mani]Adding:Adding new project of Nunit Test Project to the exixting .Net Console project

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static NUnitTesting.Program;
+using static StudentGrades.Program;
 
-namespace NUnitTesting
+namespace StudentGrades
 {
     internal class Program
     {
@@ -23,43 +23,7 @@ namespace NUnitTesting
              Console.WriteLine($"Student Grade : {grade}");
 
         }
-        public class GradeCalculator
-        {
-
-
-            public string GetGradeByPercentage(int percentage)
-            {
-
-                if (percentage >= 90 & percentage <= 100)
-                {
-                       return "A";
-                }
-               else if (percentage >= 80 && percentage <= 90)
-               {
-
-                    return "B";
-               }
-               else if (percentage >= 70 && percentage <= 80)
-               {
-
-                    return "C";
-               }
-                else if (percentage >= 60 && percentage <= 70)
-                { 
-                    return "D";
-                }
-
-                
-                else if (percentage >= 0 && percentage <= 60)
-                { 
-                    return "F";
-                }
-
-                else
-
-                { return "Invalid"; }
-            }
-        }
+        
     }
 }
 
