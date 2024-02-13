ea50f25d5aa7d137732b241e4afb62d6a49c3a63
Um6253 <um6253@srmist.edu.in>
2024-02-13 15:10:14 +0530

    [Mani]Adding:Implemented Nunit Testing and evaluating test cases

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Nunit_Testing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             var gradeCalculator = new GradeCalculator();
+
+            Console.Write("Enter the percentage : ");
+
+            var percentage = Convert.ToInt32(Console.ReadLine()); 
+
+            var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+            // Print grade
+            Console.WriteLine($"Student Grade : {grade}");
+        }
+    }
+}
