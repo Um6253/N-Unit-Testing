3a915653c7bc99b1bb803241f1a698ef51e0e693
Um6253 <um6253@srmist.edu.in>
2024-02-13 15:47:23 +0530

    [Mani]Implementing:Assign/Arrange,Act and Assert Concepts

--- Class1.cs
+++ Class1.cs
@@ -15,17 +15,17 @@ namespace Nunit_Testing
 
                 return "A";
 
-                else if (percentage >= 80 && percentage <= 90)
+            else if (percentage >= 80 && percentage <= 90)
 
                 return "B";
 
-                else if (percentage >= 70 && percentage <= 80)
+            else if (percentage >= 70 && percentage <= 80)
 
 
 
                 return "C";
 
-                else if (percentage >= 60 && percentage <= 70)
+            else if (percentage >= 60 && percentage <= 70)
 
                 return "D";
 
